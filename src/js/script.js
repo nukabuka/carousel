00d6c3630d55aa298fea529c5aae4129b9c96826
nukabuka <nukaandmaga@gmail.com>
2021-12-02 23:11:22 +0300

    skript added not seted

--- src/js/script.js
+++ src/js/script.js
@@ -16,4 +16,18 @@ $(document).ready(function(){
             }
         ]
       });
+
+          $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+            $(this)
+              .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+              .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+          });
+          $('.catalog-item__link').each(function(i){
+                $(this).on('click', function(e){
+                    e.preventDefault();
+                    $('.catalog-item__face').eq(i).toggleClass('catalog-item__face_active');
+                    $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                })
+          })
+          
   });
