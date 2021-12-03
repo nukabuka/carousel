ddecb40b90109dff79d29f90ca3d6c1bdbd2fb68
nukabuka <nukaandmaga@gmail.com>
2021-12-03 20:35:27 +0300

    mobile adated 1

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,28 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         infinite: true,
+        adaptiveHeight: true,
         speed: 800,
         slidesToShow: 1,
         // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"> <img src="icons/left.png"> </button>',
         nextArrow: '<button type="button" class="slick-next"> <img src="icons/right.png"> </button>',
         responsive: [
-            {
-                breakpoint: 768,
+            {   
+              
+
+
+                breakpoint: 992,
                 settings: {
                   dots: true,
                   arrows: false
+                
                 }
+                
             }
         ]
+      
+
       });
 
           $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
