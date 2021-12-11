fcd674351f8d7f97acbc050442f1d10f6cbf3b60
nukabuka <nukaandmaga@gmail.com>
2021-12-11 21:54:18 +0300

    madals

--- src/js/script.js
+++ src/js/script.js
@@ -42,5 +42,23 @@ $(document).ready(function(){
       };
         taggleSlide('.catalog-item__link');
         taggleSlide('.catalog-item__back');
+
+    //moduls
+    $('[data-modul=consultation]') .on('click' , function(){
+        $('.overlay, #consultation').fadeIn();
+    });
+
+    $('.button_tab').each(function(i){
+      $(this).on('click', function(){
+          $('#order .modal__deskr').text($('.catalog-item__subtitle').eq(i).text());
+          $('.overlay, #order').fadeIn();
+      })
+     });
+
+
+    $('.modal__close') .on('click' , function(){
+      $('.overlay, #consultation , #order , #thanks').fadeOut();
+    });
+
           
   });
