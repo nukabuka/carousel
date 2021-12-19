61ae2c988b4541b7a156899b7f6082ca91e29c03
nukabuka <nukaandmaga@gmail.com>
2021-12-19 18:03:51 +0300

    js

--- src/js/script.js
+++ src/js/script.js
@@ -47,6 +47,7 @@ $(document).ready(function(){
     $('[data-modul=consultation]') .on('click' , function(){
         $('.overlay, #consultation').fadeIn();
     });
+  
 
     $('.button_tab').each(function(i){
       $(this).on('click', function(){
@@ -60,5 +61,72 @@ $(document).ready(function(){
       $('.overlay, #consultation , #order , #thanks').fadeOut();
     });
 
-          
+    //volidation
+   
+   
+      
+
+    function validateForms(form){
+      $(form).validate({
+          rules: {
+              name: {
+                  required: true,
+                  minlength: 2
+              },
+              phone: "required",
+              email: {
+                  required: true,
+                  email: true
+              }
+          },
+          messages: {
+              name: {
+                  required: "Пожалуйста, введите свое имя",
+                  minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+              phone: "Пожалуйста, введите свой номер телефона",
+              email: {
+                required: "Пожалуйста, введите свою почту",
+                email: "Неправильно введен адрес почты"
+              }
+          }
+      });
+     };   
+     
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+    
+    $('form').submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            date: $(this).serialize()
+        }). done(function(){
+          $(this).find("input").val("");
+          $('#consultation, #order ').fadeOut();
+          $('.overlay, #thanks ').fadeIn('slow'); 
+          $('form').trigger('reset');
+
+        });
+        return false;
+    });
+    //up
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 1600){
+          $('.up').fadeIn();
+      }
+      else {
+          $('.up').fadeOut();
+      }
+    });
+    $('.up').click(function() {
+      $('html, body').animate({scrollTop: 0},500);
+      return false;
+    })
+
+
   });
